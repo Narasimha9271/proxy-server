dc38b7c08b49d8d14737fd05e02d98c20d03106f
Narasimha <Narasimhapalla729@gmail.com>
2024-07-12 07:23:50 +0530

    axios added

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const fetch = require("cross-fetch");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.use(cors());
+
+app.get("/api/restaurants", async (req, res) => {
+    const { lat, lng, page_type } = req.query;
+    console.log("Received query:", req.query);
+
+    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=${page_type}`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            },
+        });
+        console.log("Response from Swiggy API:", response.data);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching data from Swiggy API:", error.message);
+        if (error.response) {
+            console.error("Status:", error.response.status);
+            console.error("Data:", error.response.data);
+            res.status(error.response.status).send(error.response.data);
+        } else {
+            res.status(500).send("An error occurred");
+        }
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Proxy server is running on port ${PORT}`);
+});
 
 // app.get("/api/dapi/restaurants/list/v5", async (req, res) => {
 //     try {
@@ -28,37 +59,3 @@ app.use(cors());
 //         res.status(500).json({ error: "Failed to fetch data from Swiggy API" });
 //     }
 // });
-
-app.get("/api/restaurants", async (req, res) => {
-    const { lat, lng, page_type } = req.query;
-    console.log(req.query);
-
-    const url = `https://www.swiggy.com/api/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
-
-    await fetch(url, {
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-        },
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            res.json(data);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send("An error occurred");
-        });
-});
-
-app.listen(PORT, () => {
-    console.log(`Proxy server is running on port ${PORT}`);
-});
